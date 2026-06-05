11894c51cca40c4b72a4cbdcce04da246403dbab
Killuaaazin <josericardopedrosafilho1972@gmail.com>
2026-06-05 20:48:07 +0000

    inicio dos estudos sobre python, conclusão do modulo 1 e atividade sobre if/else feita

---
+++ atividade-mod1.py
@@ -0,0 +1,27 @@
+def switch(numero):
+    if numero == '1':
+        print ("é o numero 1")
+    elif numero == '2':
+        print ("é o numero 2")
+    elif numero == '3':
+        print ("é o numero 3")
+    elif numero == '4':
+        print ("é o numero 4")
+    elif numero == '5':
+        print ("é o numero 5")
+    elif numero == '6':
+        print ("é o numero 6")
+    elif numero == '7':
+        print ("é o numero 7")
+    elif numero == '8':
+        print ("é o numero 8")
+    elif numero == '9':
+        print ("é o numero 9")
+    else:
+        print ("numero invalido")
+    
+numero = input("escreva um número: ")
+switch(numero)
+    
+    
+    
